c73056ea7bc7defe5e116e7214193cbe6cd44633
gleb pavlov <pavlovgleb007@mail.ru>
2020-05-05 11:44:30 +0300

    initial

---
+++ testGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
